29b7316fad40042345ecfb7c2eec0d21521302d4
justlucasgomes <lucasgomes.dev@hotmail.com>
2023-09-07 16:19:54 -0300

    Terminei Aula 11 - Condições parte 1 de JavaScript
    Falamos e fizemos exercicios html e js com função if e else

---
+++ Aula11 - Condições Parte 1/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 78.2
+console.log(`A velocidade do seu carro é ${velocidade}Km/h`)
+if (velocidade > 60) { //Condição Simples
+    console.log('Você ultrapassou o limite da via, eu lamento mas foi multado!') 
+}
+console.log('Dirija sempre usando cinto de segurança!')
